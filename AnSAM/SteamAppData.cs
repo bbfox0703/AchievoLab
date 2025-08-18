72b83b514c53a60896a29a2632d2edfd20712d0d
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-08-18 16:21:41 +0800

    Use Steam metadata to resolve game images

--- AnSAM/SteamAppData.cs
+++ AnSAM/SteamAppData.cs
@@ -1,11 +1,10 @@
 using System;
-using System.Collections.Generic;
 
 namespace AnSAM
 {
     public record SteamAppData(int AppId,
                                string Title,
-                               IReadOnlyList<string>? CoverUrls = null,
+                               string? CoverUrl = null,
                                string? ExePath = null,
                                string? Arguments = null,
                                string? UriScheme = null);
