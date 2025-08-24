d7e6b2e677a42218613dcb7148653a1f8fd98e10
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-08-24 10:55:50 +0800

    Use concurrent dictionary for image cache

--- AnSAM.Tests/GameImageServiceTests.cs
+++ AnSAM.Tests/GameImageServiceTests.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,12 +16,12 @@ public class GameImageServiceTests
         var tracker = new ImageFailureTrackingService();
         var appId = Random.Shared.Next(900000, 1000000);
         tracker.RemoveFailedRecord(appId, "english");
-        var oldTime = DateTime.Now.AddDays(-1);
+        var oldTime = DateTime.Now.AddDays(-20);
         tracker.RecordFailedDownload(appId, "english", failedAt: oldTime);
 
         var service = new GameImageService();
         var cacheField = typeof(GameImageService).GetField("_imageCache", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-        var dict = (Dictionary<string, string>)cacheField!.GetValue(service)!;
+        var dict = (ConcurrentDictionary<string, string>)cacheField!.GetValue(service)!;
         var cacheKey = $"{appId}_english";
         var invalidPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".bin");
         File.WriteAllText(invalidPath, "not an image");
