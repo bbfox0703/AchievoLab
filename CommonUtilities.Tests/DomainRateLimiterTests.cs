b62f38b77ae2e76b64225dcc517365fe5c9e9887
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-08-25 13:08:34 +0800

    Propagate cancellation in rate limiter

---
+++ CommonUtilities.Tests/DomainRateLimiterTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using CommonUtilities;
+using Xunit;
+
+public class DomainRateLimiterTests
+{
+    [Fact]
+    public async Task WaitAsync_Cancellation_ReleasesSemaphore()
+    {
+        var type = typeof(GameImageCache).Assembly.GetType("CommonUtilities.DomainRateLimiter", true)!;
+        var limiter = Activator.CreateInstance(type, new object[] { 1, 60d, 1d, 60d, TimeSpan.FromMilliseconds(200), 0d })!;
+        var uri = new Uri("http://example.com");
+        var recordCall = type.GetMethod("RecordCall")!;
+        recordCall.Invoke(limiter, new object[] { uri, true, null });
+
+        var waitMethod = type.GetMethod("WaitAsync", new[] { typeof(Uri), typeof(CancellationToken) })!;
+
+        using var cts = new CancellationTokenSource(50);
+        var sw = Stopwatch.StartNew();
+        var t1 = (Task)waitMethod.Invoke(limiter, new object[] { uri, cts.Token })!;
+        await Assert.ThrowsAsync<TaskCanceledException>(async () => await t1);
+        sw.Stop();
+        Assert.InRange(sw.ElapsedMilliseconds, 0, 500);
+
+        var sw2 = Stopwatch.StartNew();
+        await (Task)waitMethod.Invoke(limiter, new object[] { uri, CancellationToken.None })!;
+        sw2.Stop();
+        Assert.InRange(sw2.ElapsedMilliseconds, 100, 500);
+
+        recordCall.Invoke(limiter, new object[] { uri, true, null });
+    }
+}
