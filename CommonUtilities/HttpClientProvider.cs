ccb74da69f8e894fea978f049c2356616191ac08
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-08-29 15:10:14 +0800

    Modify user-agent

--- CommonUtilities/HttpClientProvider.cs
+++ CommonUtilities/HttpClientProvider.cs
@@ -16,7 +16,7 @@ public static class HttpClientProvider
         };
         if (!http.DefaultRequestHeaders.Contains("User-Agent"))
         {
-            http.DefaultRequestHeaders.Add("User-Agent", "AchievoLab/1.0");
+            http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36");
         }
         return http;
     });
